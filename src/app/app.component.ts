994f6e29d7fa414f666dfcbc711453b54f5cd996
Parikshit Singh <pk223933@gmail.com>
2020-07-23 16:06:18 +0530

    removed game component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+function log(target, name, descriptor) {
+  console.log(target, name, descriptor);
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'game';
+  @log
+  aSimpleMethod() {
+    console.log('hey there!');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GameBoxComponent } from './game-box/game-box.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GameBoxComponent
   ],
   imports: [
     BrowserModule,
