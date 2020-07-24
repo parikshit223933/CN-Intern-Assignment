72087b3a17d705a75c8087e58aa5d01df2298a4a
Parikshit Singh <pk223933@gmail.com>
2020-07-24 12:20:54 +0530

    score functionality added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,11 +11,13 @@ function log(target, name, descriptor) {
 })
 export class AppComponent {
     //globals
-    arr = []
-    grid = [[0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0]]
+    arr = []//this will be rendered by the html
+    score=0;//combiner is handling this score
+    grid = [//this is for the program to use
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]]
     constructor() {
         // console.table(this.grid);
         this.addNumber();
@@ -27,10 +29,7 @@ export class AppComponent {
             //w=87
             //a=65
             //s=83
-
-
-
-            if(event.keyCode==87)
+            if (event.keyCode == 87)//moving up
             {
                 this.grid = this.rotate(this.grid);
                 this.grid = this.flip(this.grid);
@@ -38,15 +37,15 @@ export class AppComponent {
                 this.grid = this.flip(this.grid);
                 this.grid = this.rotate(this.grid);
                 this.reflectChanges();
-                
+
             }
-            if (event.keyCode == 83) {
+            if (event.keyCode == 83) {//moving down
                 this.grid = this.rotate(this.grid);
                 this.run();
                 this.grid = this.rotate(this.grid);
                 this.reflectChanges();
             }
-            if (event.keyCode == 65) {
+            if (event.keyCode == 65) {//moving left
                 this.grid = this.flip(this.grid);
                 /* this.run();
                 this.flip(this.grid); */
@@ -54,7 +53,7 @@ export class AppComponent {
                 this.grid = this.flip(this.grid);
                 this.reflectChanges();
             }
-            if (event.keyCode == 68) {
+            if (event.keyCode == 68) {//moving right
                 this.run();
                 this.reflectChanges();
 
@@ -78,25 +77,25 @@ export class AppComponent {
         return newGrid;
     }
 
-    flip = (array) => {
+    flip = (array) => {//flipping the array horizontally
         for (let i = 0; i < 4; i++) {
             array[i].reverse();
         }
         return array;
     }
-    run = () => {
+    run = () => {// operating on the grid i.e. sliding, combining and then sliding again and finally inserting a number in the array
         for (let i = 0; i < 4; i++) {
             this.grid[i] = this.operate(this.grid[i]);
         }
         this.addNumber();
     }
-    operate = (row) => {
+    operate = (row) => { //to operate
         row = this.slide(row);
         row = this.combiner(row);
         row = this.slide(row);
         return row;
     }
-    reflectChanges = () => {
+    reflectChanges = () => {//clearing the current array and pushing the new elements in the array
         this.arr = [];
         for (let row of this.grid) {
             for (let elem of row) {
@@ -104,7 +103,7 @@ export class AppComponent {
             }
         }
     }
-    slide = (row) => {
+    slide = (row) => {//this will always slide the array to the right
         let newRow = row.filter(value => value);
         let missing = 4 - newRow.length;
         let zeros = Array(missing).fill(0);
@@ -113,19 +112,21 @@ export class AppComponent {
         }
         return zeros;
     }
-    combiner = (row) => {
+    combiner = (row) => {//this will combine two adjacent number, here combination means adding
         for (let i = 3; i >= 0; i--) {
             let a = row[i];
             let b = row[i - 1];
             if (a == b) {
                 row[i] += row[i - 1];
+                //now the score will the a+b, i.e. the summed up score of the two adjacent numbers.
+                this.score+=row[i]//or we can also write a+b
                 row[i - 1] = 0
             }
         }
         return row;
     }
-    addNumber = () => {
-        let options = [];
+    addNumber = () => {//this will insert either 2 or 4 in the array at some random place
+        let options = [];//this should check for any available empty space in the grid
         for (let i = 0; i < 4; i++) {
             for (let j = 0; j < 4; j++) {
                 if (this.grid[i][j] == 0)//this is a valid spot
