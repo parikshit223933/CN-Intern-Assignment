950d3a53bbfc929f583d0a38db3db6ad284681c9
Parikshit Singh <pk223933@gmail.com>
2020-07-24 15:44:14 +0530

    touch implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as Moment from 'moment';
+import ZingTouch from 'zingtouch';
 
 function log(target, name, descriptor) {
     console.log(target, name, descriptor);
@@ -11,19 +13,29 @@ function log(target, name, descriptor) {
 })
 export class AppComponent {
     //globals
-    arr = []//this will be rendered by the html
-    score = 0;//combiner is handling this score
+    arr = [];//this will be rendered by the html
+    score: number = 0;//combiner is handling this score
     grid = [//this is for the program to use
         [0, 0, 0, 0],
         [0, 0, 0, 0],
         [0, 0, 0, 0],
         [0, 0, 0, 0]];
-    gameOver = false;
+    gameOver: boolean = false;
+    scores = [];
 
     constructor() {
+        if (!JSON.parse(localStorage.getItem("scores"))) {
+            localStorage.setItem("scores", JSON.stringify(this.scores));
+        }
+        else {
+            this.scores = JSON.parse(localStorage.getItem("scores"));
+        }
+
+        this.enableGestureControl();
+
         // console.table(this.grid);
-        this.addNumber();
-        this.addNumber();
+        this.insertNumber();
+        this.insertNumber();
         // console.table(this.grid);
         document.addEventListener('keydown', (event) => {
             //before performing any action i need to check that game is over or not
@@ -31,10 +43,20 @@ export class AppComponent {
             {
                 return;
             }
+            if (this.isWon()) {
+                window.alert('Congrats, You have won the game!');
+                this.gameOver = true;
+                return;
+            }
             if (this.isGameOver())//this is to show the alert the first time when the game is over
             {
                 this.gameOver = true;
                 window.alert('Game Over!');
+                if (this.scores.length == 10) {
+                    this.scores.pop();
+                }
+                this.scores.unshift({ time: Moment().format("ddd, h:mm:ss a"), score: this.score });
+                localStorage.setItem("scores", JSON.stringify(this.scores));
                 return;
             }
             //Here i have to declare key bindings.
@@ -71,8 +93,80 @@ export class AppComponent {
             }
         })
         this.reflectChanges();
-    }
 
+    }
+    enableGestureControl = () => {
+        setTimeout(() => {
+            let containerElement = document.getElementById('center-main');
+            let activeRegion = ZingTouch.Region(document.getElementsByClassName('container-fluid')[0]);
+            activeRegion.bind(containerElement, 'swipe', (event) => {
+                let angle = event.detail.data[0].currentDirection;
+                console.log(angle);
+                ////////////////////////////////////////////////////////////////////////////////////
+                //before performing any action i need to check that game is over or not
+                if (this.gameOver)//this is just to return the subsequest time when the game is over and player is still pressing some keys. as i dont wnat to show the alert again and again.
+                {
+                    return;
+                }
+                if (this.isWon()) {
+                    window.alert('Congrats, You have won the game!');
+                    this.gameOver = true;
+                    return;
+                }
+                if (this.isGameOver())//this is to show the alert the first time when the game is over
+                {
+                    this.gameOver = true;
+                    window.alert('Game Over!');
+                    if (this.scores.length == 10) {
+                        this.scores.pop();
+                    }
+                    this.scores.unshift({ time: Moment().format("ddd, h:mm:ss a"), score: this.score });
+                    localStorage.setItem("scores", JSON.stringify(this.scores));
+                    return;
+                }
+                if (angle >= 45 && angle <= 135)//moving up
+                {
+                    this.grid = this.rotate(this.grid);
+                    this.grid = this.flip(this.grid);
+                    this.run();
+                    this.grid = this.flip(this.grid);
+                    this.grid = this.rotate(this.grid);
+                    this.reflectChanges();
+                }
+                if (angle >= 225 && angle <= 315) {//moving down
+                    this.grid = this.rotate(this.grid);
+                    this.run();
+                    this.grid = this.rotate(this.grid);
+                    this.reflectChanges();
+                }
+                if (angle >= 135 && angle <= 225) {//moving left
+                    this.grid = this.flip(this.grid);
+                    /* this.run();
+                    this.flip(this.grid); */
+                    this.run();
+                    this.grid = this.flip(this.grid);
+                    this.reflectChanges();
+                }
+                if ((angle <= 45 && angle >= 0) || (angle >= 315 && angle <= 360)) {//moving right
+                    this.run();
+                    this.reflectChanges();
+                }
+                ///////////////////////////////////////////////////////////////////////////////////
+            });
+        }, 1000)
+
+        // 
+    }
+    isWon = () => {
+        for (let i = 0; i < 4; i++) {
+            for (let j = 0; j < 4; j++) {
+                if (this.grid[i][j] == 2048) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
     isGameOver = () =>//here i have to check if i do not have any adjacent units containing same numbers and the board is full, in that case the game will be over!
     {
         for (let i = 0; i < 4; i++) {
@@ -90,16 +184,15 @@ export class AppComponent {
         }
         return true;
     }
-    restartGame=()=>
-    {
-        this.score=0;
-        this.gameOver=false;
-        this.grid=[
+    restartGame = () => {
+        this.score = 0;
+        this.gameOver = false;
+        this.grid = [
             [0, 0, 0, 0],
             [0, 0, 0, 0],
             [0, 0, 0, 0],
             [0, 0, 0, 0]];
-        this.arr=[];
+        this.arr = [];
         this.run();
         this.reflectChanges()
     }
@@ -129,7 +222,7 @@ export class AppComponent {
         for (let i = 0; i < 4; i++) {
             this.grid[i] = this.operate(this.grid[i]);
         }
-        this.addNumber();
+        this.insertNumber();
     }
 
     operate = (row) => { //to operate
@@ -172,7 +265,7 @@ export class AppComponent {
         return row;
     }
 
-    addNumber = () => {//this will insert either 2 or 4 in the array at some random place
+    insertNumber = () => {//this will insert either 2 or 4 in the array at some random place
         let options = [];//this should check for any available empty space in the grid
         for (let i = 0; i < 4; i++) {
             for (let j = 0; j < 4; j++) {
